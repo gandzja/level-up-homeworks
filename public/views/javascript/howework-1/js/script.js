e461bce38956f59e234adf1df4e11e0faea1d868
gandzja <gandzja@gmail.com>
2019-06-13 18:19:02 +0300

    JS HW 1

---
+++ public/views/javascript/howework-1/js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var admin;
+var name;
+name = 'Vasiliy';
+admin = name;
+console.log(admin);
+
+var nameOurPlanet;
+var nameVisitor;
+
+var framePerSecond;
+var connectorBetweenWords;
+var docomentView;
+var appearenceLastPage;
